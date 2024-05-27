f8d51d393504769cafb715c9e43a4e43ab38d4a2
Xander Voorvaart <xvoorvaa@bess-f2r3s9.clusters.42paris.fr>
2024-05-27 12:28:28 +0200

    Added documentation to strcpy

--- src/assembly/core/ft_strcpy.s
+++ src/assembly/core/ft_strcpy.s
@@ -1,23 +1,24 @@
-BITS 64
+	BITS 64
 
-section .text
-  global ft_strcpy
+	section .text
+	global  ft_strcpy
 
-; rdi == dest*
-; rsi == src*
+	; rdi == dest*
+	; rsi == src*
 
 ft_strcpy:
-  mov rcx, 0
+	xor rcx, rcx
 
 l1:
-  mov al, [rsi + rcx]
-  mov [rdi + rcx], al
-  cmp al, 0
-  je end
-  inc rcx
-  jmp l1
+	;   mov src + count to `al`, `al` being a BYTE and then move it to dest
+	mov al, [rsi + rcx]
+	mov [rdi + rcx], al
+	cmp al, 0
+	je  end
+
+	inc rcx
+	jmp l1
 
 end:
-  mov rax, rdi
-  ret
-  
+	mov rax, rdi
+	ret
